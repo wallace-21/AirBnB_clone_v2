e7e4936c1edd637f38025cd2a9bc24fa1c4210e5
wallace <exnesswallace@gmail.com>
2024-02-19 01:39:32 +0200

    script that starts a Flask web application that can be accessed through 3 routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" import flask module"""
+from flask import Flask
+
+app = Flask(__name__)
+"""create an instance of flask"""
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ function that prints a string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text = escape(text.replace('_', ' '))
+    return ("C {}".format(text))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
