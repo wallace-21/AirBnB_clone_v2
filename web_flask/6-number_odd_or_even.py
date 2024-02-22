baad018d558a2800de57dd71664bc69c395e6daa
wallace <exnesswallace@gmail.com>
2024-02-22 23:45:37 +0200

    fixed odd from docstring

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,7 +44,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    """function thaat print even or od"""
+    """function thaat print even or odd"""
     if isintance(n, int):
         even_or_odd = "even" if n % 2 == 0 else "odd"
         return render_template('6-number_odd_or_even.html',
